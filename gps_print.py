b14f94c8087e77ecef063264ccaf99431f8b9cca
Arijit Das <arijit1080@gmail.com>
2022-02-26 12:41:23 +0530

    create file to print the GPS data

---
+++ gps_print.py
@@ -0,0 +1,15 @@
+import serial
+import pynmea2
+
+while True:
+        port="/dev/ttyAMA0"
+        ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+        dataout = pynmea2.NMEAStreamReader()
+        newdata=ser.readline()
+        n_data = newdata.decode('latin-1')
+        if n_data[0:6] == '$GPRMC':
+                newmsg=pynmea2.parse(n_data)
+                lat=newmsg.latitude
+                lng=newmsg.longitude
+                gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
+                print(gps)
